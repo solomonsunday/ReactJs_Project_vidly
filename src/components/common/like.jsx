3ea4e72700f7be2012d7b9c55279258167ac36a9
unknown <richard.okoye@netopng.com>
2020-01-09 21:09:42 +0100

    Likes was added here

---
+++ src/components/common/like.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+// imput of this component is liked: boolean
+// output:onClick.
+
+const Like = props => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
+
+// class Like extends Component {
+//   render() {
+//     let classes = "fa fa-heart";
+//     if (!this.props.liked) classes += "-o";
+//     return (
+//       <i
+//         onClick={this.props.onClick}
+//         style={{ cursor: "pointer" }}
+//         className={classes}
+//         aria-hidden="true"
+//       ></i>
+//     );
+//   }
+// }
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = {
     movies: getMovies()
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 
   handleDelete = movie => {
@@ -25,6 +34,7 @@ class Movies extends Component {
               <th>Genre </th>
               <th>Stock </th>
               <th>Rate </th>
+              <th>Like</th>
               <th>Actions </th>
             </tr>
           </thead>
@@ -35,6 +45,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
